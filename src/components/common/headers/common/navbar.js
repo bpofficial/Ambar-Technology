fc967281e4aa37e13ec5c71cd53f2c08e8583421
Brayden Phillips <bpofficial@icloud.com>
2019-07-07 21:51:55 +1000

    Migrated to functional component

---
+++ src/components/common/headers/common/navbar.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import {Link} from 'react-router-dom';
+import $ from 'jquery';
+import 'smartmenus';
+
+export default (props, state) => {
+
+    $(function() {
+        $('#main-menu').smartmenus({
+            subMenusSubOffsetX: 1,
+            subMenusSubOffsetY: -8
+        });
+    });
+    if (window.innerWidth < 750) {
+        this.setState({navClose: {right:'-410px'}})
+    }
+    if (window.innerWidth < 1199) {
+        this.setState({navClose: {right:'-300px'}})
+    }
+
+    const [navState, setNavState] = useState({right: '0px'})
+
+    const openNav = () =>  {
+        setNavState({right:'0px'})
+    }
+
+    const closeNav = () =>  {
+        setNavState({right:'-410px'})
+    }
+
+    const onMouseEnterHandler = () => {
+        if (window.innerWidth > 1199) {
+            document.querySelector("#main-menu").classList.add("hover-unset");
+        }
+    }
+
+    return (
+        <div>
+            <nav id="main-nav">
+                <div className="toggle-nav" onClick={openNav.bind(this)}>
+                    <i className="fa fa-bars sidebar-bar" ></i>
+                </div>
+                <ul id="main-menu" className="sm pixelstrap sm-horizontal" style={navState}>
+                    <li >
+                        <div className="mobile-back text-right" onClick={closeNav.bind(this)}>
+                            Back
+                            <i className="fa fa-angle-right pl-2" aria-hidden="true" />
+                        </div>
+                    </li>
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
+                    <li>
+                        <a  style={{paddingRight:'75px'}} href="/store">Store</a>
+                        <ul>
+                            {/* TODO: Complete this => Category collection */}
+                            <li><Link to={`${process.env.PUBLIC_URL}/store`} >{'Category 1'}</Link></li>
+                        </ul>
+                    </li>
+                    <li >
+                        <a href="/blog">Blog</a>
+                    </li>
+                    <li />
+                    <li >
+                        <a href="/about">About Us</a>
+                    </li>
+                    <li >
+                        <a href="/contact">Contact</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
