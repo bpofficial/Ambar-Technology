5923a84c03b12ed7c8301b77a67852ed684c2c16
Brayden Phillips <bpofficial@icloud.com>
2019-07-17 12:10:25 +1000

    Refactored to a single input/output class.

--- Server/src/Modules/Payment/Class.ts
+++ Server/src/Modules/Payment/Class.ts
@@ -1,8 +1,9 @@
 import { Typegoose, prop } from "typegoose";
 import { ObjectType, Field, InputType } from "type-graphql";
 
+
+@InputType('PaymentInput')
 @ObjectType({ description: "Class representing Payment." })
-@InputType()
 export default class Payment extends Typegoose {
 
     @Field(type => String)
