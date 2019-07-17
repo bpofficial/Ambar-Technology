9924dfbf38ef475a4c0da55739602842735827b9
Brayden Phillips <bpofficial@icloud.com>
2019-07-17 12:11:48 +1000

    Changed from error stack to error message.

--- Server/src/Common/Services/Redis.ts
+++ Server/src/Common/Services/Redis.ts
@@ -3,5 +3,5 @@ const redis = require("redis")
 export const client = redis.createClient(process.env.REDIS_URL || undefined);
 
 client.on("error", (err) => {
-    console.log("CAUGHT: [redis]", err)
+    console.log("CAUGHT: [redis]", err.message)
 });
